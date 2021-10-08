39a4fb97e09787a0842a401638ec03634d08daa6
luke.inger@jcb-finance.co.uk <luke.inger@jcb-finance.co.uk>
2021-10-08 13:11:44 +0100

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Luke Inger on 08/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
